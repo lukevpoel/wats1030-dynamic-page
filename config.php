1e6505409d5fb5752d928d9379c469b2a7f1d4c6
Luke Vander Poel <airscout99@gmail.com>
2016-07-31 21:42:05 -0700

    updated with correct info

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$API_KEY = 'Put your API key here'; // This will be your Flickr-issued API Key.
-$PAGE_TITLE = 'Kittens and Rainbows'; // This should be a user-friendly page title.
-$SEARCH_TAGS = 'kitten,rainbow'; // This must be a comma-separated list
+$API_KEY = '9fbba41e75d9842a8915cc27b029af71'; // This will be your Flickr-issued API Key.
+$PAGE_TITLE = 'Puppies!'; // This should be a user-friendly page title.
+$SEARCH_TAGS = 'puppies,puppy,dog'; // This must be a comma-separated list
 
 ?>
